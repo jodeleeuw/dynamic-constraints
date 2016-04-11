716780bab37577e8af0fb89861fcf292e820877c
Josh de Leeuw <josh.deleeuw@gmail.com>
2016-04-11 17:51:32 -0400

    modeling experiment 2b

---
+++ letters-learn-then-test/modeling/sequence-generators.R
@@ -0,0 +1,30 @@
+#### sequence generators ####
+
+create_base_sequence <- function(reps){
+  items <- 1:4
+  s <- sample(items,4)
+  for(i in 2:reps){
+    p <- sample(items,4)
+    while(p[1] == s[length(s)]){
+      p <- sample(items,4)
+    }
+    s <- c(s,p)
+  }
+  return(s)
+}
+
+four_triple_sequence <- function(reps){
+  triples <- c('ABC','DEF','GHI','JKL')
+  s <- create_base_sequence(reps)
+  s2 <- sapply(s, function(i){ return(triples[i])})
+  final <- paste0(s2, collapse="")
+  return(final)
+}
+
+seed_sequence <- function(reps){
+  triples <- c('DEF','GHI','JKL')
+  s <- sample(1:3, reps*3, replace=T)
+  s2 <- sapply(s, function(i){ return(triples[i])})
+  final <- paste0(s2, collapse="")
+  return(final)
+}
